2048c5ba8b9fb67469cdb04ab287e12f2f860a4e
hervegross <hgross@ar2tech.com>
2014-02-04 12:38:05 -0800

    remove multiples from a vector  distribution

--- ar2gems/grid/distribution_utilities.cpp
+++ ar2gems/grid/distribution_utilities.cpp
@@ -22,7 +22,8 @@
 ** sourceforge.net/projects/sgems.
 ** ----------------------------------------------------------------------------*/
 
-
+// to remove multiple values
+#include <algorithm>    // std::lower_bound, std::upper_bound, std::sort
 
 #include <grid/distribution_utilities.h>
 #include <utils/manager.h>
@@ -122,39 +123,76 @@ Non_parametric_distribution*
     if(dist == 0) return 0;
   }
 
-  typedef std::pair<float, float> z_p_pair_type;
+  typedef std::pair<float, double> z_p_pair_type;
   std::vector<z_p_pair_type> values;
-
   for(int i=0; i<prop->size(); ++i) {
     if( !prop->is_informed(i) || !weight->is_informed(i) ) continue;
-
-    float w = weight->get_value(i);
+    double w = weight->get_value(i);
     if(w == 0) continue;
     values.push_back(  std::make_pair(prop->get_value(i), w) );
   }
-
   std::sort(values.begin(), values.end()); // Sorting on pairs works on the first element
 
   std::vector<float> z;
-  std::vector<float> p;
-  z.reserve(values.size());
-  p.reserve(values.size());
-  std::vector<z_p_pair_type>::const_iterator it = values.begin();
-  float sum_w = 0.0;
-  for( ; it != values.end() ; ++it) {
+  std::vector<double> p;
+  std::vector<z_p_pair_type>::iterator low,up,it;
+  it=values.begin();
+  while(it != values.end()) {
+    z_p_pair_type last_z_p(it->first,1.0);
+    up  = std::lower_bound(it,values.end(),last_z_p); // up is the last position of the value we look for (here: last_z_p)
     z.push_back(it->first);
-    sum_w +=it->second;
-    p.push_back(sum_w);
+    double sum_p_for_this_z = p.back();
+    for (  ; it != up ; ++it ){
+      sum_p_for_this_z +=  it->second;
+    }
+    p.push_back(sum_p_for_this_z);
+    it = up;
   }
-  if(sum_w > 1.0)  //may happen due to rounding errors
-    p.back() = 1.0;
-    
+
+  if(p.back() > 1.00 ) p.back() = 1.00;
+
   dist->initialize(z.begin(),z.end(),p.begin(), low_interp, mid_interp, up_interp);
 
   bool ok = dist->is_valid_distribution();
 
-  
-
   return dist;
+
+};
+
+
+/// \brief Sort and remove multiplets from a vector of pairs, using the first value of the pair as a key. Sum-aggregate to 1 the second values for a given key.
+/// \param Vector of pairs made of a float and a double
+/// \param[out] Vector of pairs made of a float and a double
+template <
+  typename z_type,
+  typename p_type 
+>
+ void initialize_data_for_distribution ( std::vector<std::pair<z_type,p_type> >& input_pair, std::vector<std::pair<z_type,p_type> >& output_pair) {
+
+  output_pair.clear();
+  output_pair.reserve(input_pair.size());
+  std::sort(input_pair.begin(), input_pair.end());    // Sorting on pairs works on the first element
+
+  std::vector<std::pair<z_type,p_type> >::iterator up,it;
+
+  p_type sum_p = 0.0;
+  it=input_pair.begin();
+  while(it != input_pair.end()) {
+    std::pair<z_type,p_type> last_pair(it->first,1.0);
+    up  = std::lower_bound(it,input_pair.end(),last_pair); // up is the last position of the value we look for (here: last_pair)
+    z_type z = it->first;
+    // cumulative sum of weights:
+    for (  ; it != up ; ++it ){
+      sum_p +=  it->second;
+    }
+    it = up;
+    
+    // reconstruct pair
+    output_pair.push_back(  std::make_pair(z, sum_p) );
+  }
+  
+  // Impose 1.00 as last cumulative sum value of weights/probabilities:
+  if(output_pair.back().second > 1.00 ) output_pair.back().second = 1.00;
+
 
 };
